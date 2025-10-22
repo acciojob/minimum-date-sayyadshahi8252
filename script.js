539237310fe75f8a902943c266803d6931c620a2
sayyadshahi8252 <167566299+sayyadshahi8252@users.noreply.github.com>
2025-10-22 17:12:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function minDate(dates) {
   //write you code here
+
+  let min = new Date(dates[0]);  
+  
+  for (let i = 1; i < dates.length; i++) {
+    let current = new Date(dates[i]);
+    if (current < min) {
+      min = current;
+    }
+  }
+  return dates.find(d => new Date(d).getTime() === min.getTime());
 }
+
 
 // Do not change the code
 
